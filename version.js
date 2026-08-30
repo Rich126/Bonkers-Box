5aca60c4e3f9ac686666fd4cbdaf1e9f3eb1ac43
Rich126 <143965576+Rich126@users.noreply.github.com>
2026-08-30 20:55:51 +0100

    Add files via upload

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 /* Spencer Games release marker. Update VERSION for every published build. */
 (() => {
-  const VERSION = 'v3.7';
+  const VERSION = 'v3.8';
   window.SPENCER_GAMES_VERSION = VERSION;
 
   function mountVersionBadge() {
