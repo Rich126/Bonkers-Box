7d592e4736d5e939cdcb7404afa03e6822bec57d
Rich126 <143965576+Rich126@users.noreply.github.com>
2026-08-30 14:20:38 +0100

    Add files via upload

---
+++ live/creative.js
@@ -0,0 +1,19 @@
+window.SPENCER_LIVE_CREATIVE = {
+  id: 'creative-party',
+  name: 'Creative Party',
+  description: 'Picture and sound challenges with family voting.',
+  rounds: [
+    {
+      kind: 'picture',
+      title: 'Picture Challenge',
+      prompt: "Take a picture of you looking like... YOU'VE JUST WON THE LOTTERY!",
+      captureSeconds: 60
+    },
+    {
+      kind: 'sound',
+      title: 'Sound Challenge',
+      prompt: "Mimic the sound of... A CAR THAT REALLY DOESN'T WANT TO START!",
+      captureSeconds: 45
+    }
+  ]
+};
